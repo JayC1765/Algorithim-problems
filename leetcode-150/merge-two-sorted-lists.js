3399513247dd35b8b3cf78e90a15fe290387448f
Jason Chan <jason.chann91@gmail.com>
2023-11-09 15:50:30 -0500

    completed mergeTwoLists

---
+++ leetcode-150/merge-two-sorted-lists.js
@@ -0,0 +1,44 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+*/
+
+const mergeTwoLists = (list1, list2) => {
+  const dummy = new ListNode();
+  let head = dummy;
+
+  while (list1 && list2) {
+    if (list2.val > list1.val) {
+      head.next = list1;
+      list1 = list1.next;
+    } else {
+      head.next = list2;
+      list2 = list2.next;
+    }
+
+    head = head.next;
+  }
+
+  if (list1) head.next = list1;
+  if (list2) head.next = list2;
+
+  return dummy.next;
+};
