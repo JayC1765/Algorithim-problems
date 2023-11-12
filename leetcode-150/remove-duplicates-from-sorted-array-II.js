9e9d33ef942e77da764943dc61195c4d47174cf8
Jason Chan <jason.chann91@gmail.com>
2023-11-12 18:25:34 -0500

    completed remove-duplicates-from-sorted-array-ii

--- leetcode-150/remove-duplicates-from-sorted-array-II.js
+++ leetcode-150/remove-duplicates-from-sorted-array-II.js
@@ -46,3 +46,18 @@ Explanation: Your function should return k = 7, with the first seven elements of
 being 0, 0, 1, 1, 2, 3 and 3 respectively. It does not matter what you leave beyond the 
 returned k (hence they are underscores).
 */
+
+const removeDuplicates = (nums) => {
+  if (nums.length <= 2) return nums.length;
+
+  let k = 2;
+
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] > nums[k - 2]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
